529639c29f20ae429e58d2d8d5e43bb25578958b
L. X. Li <50414011+xli2522@users.noreply.github.com>
2025-05-19 11:23:22 -0400

    bump version 0.1.1; add dist build to ignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="maguniverse",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["requests", "pandas", "jinja2"],
     author="X. Li",
