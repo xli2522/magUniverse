63df54f5f18b48e02f1d8ff9a0ef87622eb7cd1b
L. X. Li <50414011+xli2522@users.noreply.github.com>
2025-05-24 13:32:38 -0400

    avoid circular import

--- maguniverse/data/polarization/__init__.py
+++ maguniverse/data/polarization/__init__.py
@@ -1,3 +1,6 @@
+# paper sources
+from maguniverse.data.polarization.sources import polarization_sources
+# getters
 from maguniverse.data.polarization.dotson2010 import get_dotson2010
 from maguniverse.data.polarization.matthews2009 import get_matthews2009
 from maguniverse.data.polarization.harris2018 import get_harris2018
@@ -7,50 +10,3 @@ __all__ = [ "polarization_sources",
             "get_matthews2009",
             "get_harris2018"]
 
-
-polarization_sources = {
-    "Dotson2010": {
-        "title"     : "350 μm Polarimetry from the Caltech Submillimeter Observatory",
-        "authors"   : "J. L. Dotson, J. E. Vaillancourt, L. Kirby et al.",
-        "year"      : 2010,
-        "paper_link": "https://iopscience.iop.org/article/10.1088/0067-0049/186/2/406",
-        "doi"       : "10.1088/0067-0049/186/2/406",
-        "instrument": "CSO / Hertz 350 μm polarimeter",
-        "data_link" : {
-            "CDS"                 : "https://vizier.cds.unistra.fr/viz-bin/VizieR?-source=10.1088/0067-0049/186/2/406",
-            "t1_object_list_ascii": "https://iopscience.iop.org/0067-0049/186/2/406/suppdata/apjs333144t1_ascii.txt?doi=10.1088/0067-0049/186/2/406",
-            "t2_data_table_ascii" : "https://content.cld.iop.org/journals/0067-0049/186/2/406/revision1/apjs333144t2_mrt.txt",
-            "t1_object_list_local": "datafiles/polarization/dotson2010_t1.txt",
-            "t2_data_table_local" : "datafiles/polarization/dotson2010_t2.txt",
-            },
-    },
-    "Matthews2009"  : {
-        "title"     : "The Legacy of SCUPOL: 850 μm Imaging Polarimetry from 1997 to 2005",
-        "authors"   : "B. C. Matthews, C. A. McPhee, L. M. Fissel, R. L. Curran",
-        "year"      : 2009,
-        "paper_link": "https://iopscience.iop.org/article/10.1088/0067-0049/182/1/143",
-        "doi"       : "10.1088/0067-0049/182/1/143",
-        "instrument": "JCMT / SCUPOL 450 μm & 850 μm",
-        "data_link" : {
-            "CDS"               : "https://vizier.cds.unistra.fr/viz-bin/VizieR?-source=10.1088/0067-0049/182/1/143",   
-            "t1_targets"        : "https://iopscience.iop.org/0067-0049/182/1/143/suppdata/apjs300733t1_ascii.txt?doi=10.1088/0067-0049/182/1/143",
-            "t5_results_summary": "https://iopscience.iop.org/0067-0049/182/1/143/suppdata/apjs300733t5_ascii.txt?doi=10.1088/0067-0049/182/1/143",
-            "t6_polarization"   : "https://content.cld.iop.org/journals/0067-0049/182/1/143/revision1/apjs300733t6_mrt.txt",
-        },
-    },
-    "Harris2018"   : {
-        "title"    : "ALMA Observations of Polarized 872μm Dust Emission from the Protostellar Systems VLA 1623 and L1527",
-        "authors"  : "Robert J. Harris, Erin G. Cox, Leslie W. Looney, et al.",
-        "year"     : 2018,
-        "paper_link": "https://iopscience.iop.org/article/10.3847/1538-4357/aac6ec",
-        "ads"      : "https://ui.adsabs.harvard.edu/abs/2018ApJ...861...91H/abstract",
-        "doi"      : "10.3847/1538-4357/aac6ec",
-        "instrument": "ALMA Band 7",
-        "data_link" : {
-            "CDS"               : "https://simbad.cds.unistra.fr/simbad/sim-basic?Ident=2018ApJ...861...91H&submit=SIMBAD+search",
-            "t1_imaging"        : "https://iopscience.iop.org/0004-637X/861/2/91/suppdata/apjaac6ect1_ascii.txt?doi=10.3847/1538-4357/aac6ec",
-            "t2_plane_fitting"  : "https://iopscience.iop.org/0004-637X/861/2/91/suppdata/apjaac6ect2_ascii.txt?doi=10.3847/1538-4357/aac6ec",
-            "t3_polarization"   : "https://iopscience.iop.org/0004-637X/861/2/91/suppdata/apjaac6ect3_ascii.txt?doi=10.3847/1538-4357/aac6ec",
-            },
-    }
-}
--- maguniverse/data/zeeman/__init__.py
+++ maguniverse/data/zeeman/__init__.py
@@ -1,21 +1,8 @@
+# paper sources
+from maguniverse.data.zeeman.sources import zeeman_sources
+# getters
 from maguniverse.data.zeeman.crutcher2010 import get_crutcher2010
 
 __all__ = [ "zeeman_sources",
             "get_crutcher2010"]
 
-zeeman_sources = {
-    "Crutcher2010": {
-        "title"     : "Magnetic Fields in Interstellar Clouds from Zeeman Observations: Inference of Total Field Strengths by Bayesian Analysis",
-        "authors"   : "Richard M. Crutcher, Benjamin Wandelt, Carl Heiles, Edith Falgarone, and Thomas H. Troland",
-        "year"      : 2010,
-        "publisher_link": "https://iopscience.iop.org/article/10.1088/0004-637X/725/1/466",
-        "ads_link"  : "https://ui.adsabs.harvard.edu/abs/2010ApJ...725..466C/abstract",
-        "doi"       : "10.1088/0004-637X/725/1/466",
-        "instrument": "Compilation of HI, OH, and CN Zeeman measurements",
-        "note"      : "This table compiles four earlier surveys (Heiles & Troland 2004; Crutcher et al. 1999; Troland & Crutcher 2008; Falgarone et al. 2008)",
-        "data_link" : {
-            "table1_ascii": "https://iopscience.iop.org/0004-637X/725/1/466/suppdata/apj333303t1_ascii.txt?doi=10.1088/0004-637X/725/1/466",
-            "table1_local": "datafiles/zeeman/crutcher2010.txt",
-        },
-    },
-}
