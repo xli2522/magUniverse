62463bc48c8d24531ff6ef73d0c28c1e681f9f22
L. X. Li <50414011+xli2522@users.noreply.github.com>
2025-06-04 12:34:57 -0400

    bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="maguniverse",
-    version="v0.1.3",
+    version="0.1.4",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
