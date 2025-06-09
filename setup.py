320e780384b9f7eeaaf89af2cb373746a958da8f
L. X. Li <50414011+xli2522@users.noreply.github.com>
2025-06-09 01:36:41 -0400

    use SPDX license expression in setup
    See: https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license

--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ setup(
     author="X. Li",
     description="A Python-based data manager for working with tabulated data from publications of observational surveys of cosmic magnetic fields.",
     url="https://github.com/xli2522/maguniverse",
+    license="MIT",
+    license_expression="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
